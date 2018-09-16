4f2fdfcb6f2a8b0904a3c47e8ee2a7e7b7a04be5
Chris <you@example.com>
2018-09-16 18:06:36 -0400

    added

--- scraper/settings.py
+++ scraper/settings.py
@@ -1,11 +1,4 @@
 import os
 
 path = os.path.dirname(os.getcwd())
-log_path = path + '/logs/'
-
-google_uname = "DutrowLLC@Gmail.com"
-google_passwd = "Modena01"
-
-google_business_url = "https://business.google.com/u/2/insights/l/05705162593023190148"
-
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36"
+log_path = path + '/scraper/logs/'
